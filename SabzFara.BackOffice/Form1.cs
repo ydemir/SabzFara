fcb83883404258366fc9927ebf40d6ce663c9391
Yusuf Demir <yusuf@bilgiedin.net>
2018-05-20 14:06:21 +0300

    Add project files.

---
+++ SabzFara.BackOffice/Form1.cs
@@ -0,0 +1,25 @@
+﻿using SabzFara.Entities.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SabzFara.BackOffice
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (var context = new SabzFaraContext())
+            {
+                context.Database.CreateIfNotExists();
+            }
+        }
+    }
+}
