2893ee6ba57cd1fbe9c4fac1ee73f8af8362eb65
Yusuf Demir <yusuf@bilgiedin.net>
2018-05-29 01:49:54 +0300

    Kasa Seçim Formu kodlandı ve tasarlandı

---
+++ SabzFara.BackOffice/Kasa/FrmKasaSec.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace SabzFara.BackOffice.Kasa
+{
+    partial class FrmKasaSec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKasaSec));
+            this.colAciklama = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colKasaGiris = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBakiye = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridContSecim = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colKasaKodu = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colKasaAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colYetkiliKodu = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colYetkiliAdi = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colKasaCikis = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.grpMenu = new DevExpress.XtraEditors.GroupControl();
+            this.btnSec = new DevExpress.XtraEditors.SimpleButton();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnKapat = new DevExpress.XtraEditors.SimpleButton();
+            this.lblBaslik = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridContSecim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).BeginInit();
+            this.grpMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colAciklama
+            // 
+            this.colAciklama.FieldName = "Aciklama";
+            this.colAciklama.Name = "colAciklama";
+            this.colAciklama.OptionsColumn.AllowEdit = false;
+            this.colAciklama.Visible = true;
+            this.colAciklama.VisibleIndex = 4;
+            this.colAciklama.Width = 219;
+            // 
+            // colKasaGiris
+            // 
+            this.colKasaGiris.Caption = "Kasa Giriş";
+            this.colKasaGiris.FieldName = "KasaGiris";
+            this.colKasaGiris.Name = "colKasaGiris";
+            this.colKasaGiris.OptionsColumn.AllowEdit = false;
+            this.colKasaGiris.Visible = true;
+            this.colKasaGiris.VisibleIndex = 5;
+            this.colKasaGiris.Width = 77;
+            // 
+            // colBakiye
+            // 
+            this.colBakiye.Caption = "Bakiye";
+            this.colBakiye.FieldName = "Bakiye";
+            this.colBakiye.Name = "colBakiye";
+            this.colBakiye.OptionsColumn.AllowEdit = false;
+            this.colBakiye.Visible = true;
+            this.colBakiye.VisibleIndex = 7;
+            this.colBakiye.Width = 83;
+            // 
+            // gridContSecim
+            // 
+            this.gridContSecim.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridContSecim.Location = new System.Drawing.Point(0, 47);
+            this.gridContSecim.MainView = this.gridView1;
+            this.gridContSecim.Name = "gridContSecim";
+            this.gridContSecim.Size = new System.Drawing.Size(1096, 394);
+            this.gridContSecim.TabIndex = 9;
+            this.gridContSecim.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colKasaKodu,
+            this.colKasaAdi,
+            this.colYetkiliKodu,
+            this.colYetkiliAdi,
+            this.colAciklama,
+            this.colKasaGiris,
+            this.colKasaCikis,
+            this.colBakiye});
+            this.gridView1.GridControl = this.gridContSecim;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.Name = "colId";
+            this.colId.OptionsColumn.AllowEdit = false;
+            this.colId.OptionsColumn.ShowInCustomizationForm = false;
+            // 
+            // colKasaKodu
+            // 
+            this.colKasaKodu.Caption = "Kasa Kodu";
+            this.colKasaKodu.FieldName = "KasaKodu";
+            this.colKasaKodu.Name = "colKasaKodu";
+            this.colKasaKodu.OptionsColumn.AllowEdit = false;
+            this.colKasaKodu.Visible = true;
+            this.colKasaKodu.VisibleIndex = 0;
+            this.colKasaKodu.Width = 105;
+            // 
+            // colKasaAdi
+            // 
+            this.colKasaAdi.Caption = "Kasa Adı";
+            this.colKasaAdi.FieldName = "KasaAdi";
+            this.colKasaAdi.Name = "colKasaAdi";
+            this.colKasaAdi.OptionsColumn.AllowEdit = false;
+            this.colKasaAdi.Visible = true;
+            this.colKasaAdi.VisibleIndex = 1;
+            this.colKasaAdi.Width = 292;
+            // 
+            // colYetkiliKodu
+            // 
+            this.colYetkiliKodu.Caption = "Yetkili Kodu";
+            this.colYetkiliKodu.FieldName = "YetkiliKodu";
+            this.colYetkiliKodu.Name = "colYetkiliKodu";
+            this.colYetkiliKodu.OptionsColumn.AllowEdit = false;
+            this.colYetkiliKodu.Visible = true;
+            this.colYetkiliKodu.VisibleIndex = 2;
+            this.colYetkiliKodu.Width = 106;
+            // 
+            // colYetkiliAdi
+            // 
+            this.colYetkiliAdi.Caption = "Yetkili Adı";
+            this.colYetkiliAdi.FieldName = "YetkiliAdi";
+            this.colYetkiliAdi.Name = "colYetkiliAdi";
+            this.colYetkiliAdi.OptionsColumn.AllowEdit = false;
+            this.colYetkiliAdi.Visible = true;
+            this.colYetkiliAdi.VisibleIndex = 3;
+            this.colYetkiliAdi.Width = 119;
+            // 
+            // colKasaCikis
+            // 
+            this.colKasaCikis.Caption = "Kasa Çıkış";
+            this.colKasaCikis.FieldName = "KasaCikis";
+            this.colKasaCikis.Name = "colKasaCikis";
+            this.colKasaCikis.OptionsColumn.AllowEdit = false;
+            this.colKasaCikis.Visible = true;
+            this.colKasaCikis.VisibleIndex = 6;
+            this.colKasaCikis.Width = 77;
+            // 
+            // grpMenu
+            // 
+            this.grpMenu.Controls.Add(this.btnSec);
+            this.grpMenu.Controls.Add(this.btnKapat);
+            this.grpMenu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grpMenu.Location = new System.Drawing.Point(0, 441);
+            this.grpMenu.Name = "grpMenu";
+            this.grpMenu.Size = new System.Drawing.Size(1096, 74);
+            this.grpMenu.TabIndex = 8;
+            this.grpMenu.Text = "Menü";
+            // 
+            // btnSec
+            // 
+            this.btnSec.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSec.ImageOptions.ImageIndex = 0;
+            this.btnSec.ImageOptions.ImageList = this.ımageList1;
+            this.btnSec.Location = new System.Drawing.Point(919, 17);
+            this.btnSec.Name = "btnSec";
+            this.btnSec.Size = new System.Drawing.Size(83, 52);
+            this.btnSec.TabIndex = 0;
+            this.btnSec.Text = "Seç";
+            this.btnSec.Click += new System.EventHandler(this.btnSec_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "check.png");
+            this.ımageList1.Images.SetKeyName(1, "folder_out.png");
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnKapat.ImageOptions.ImageIndex = 1;
+            this.btnKapat.ImageOptions.ImageList = this.ımageList1;
+            this.btnKapat.Location = new System.Drawing.Point(1008, 17);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(83, 52);
+            this.btnKapat.TabIndex = 0;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaslik.Appearance.Image = ((System.Drawing.Image)(resources.GetObject("lblBaslik.Appearance.Image")));
+            this.lblBaslik.Appearance.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblBaslik.Appearance.Options.UseFont = true;
+            this.lblBaslik.Appearance.Options.UseImage = true;
+            this.lblBaslik.Appearance.Options.UseImageAlign = true;
+            this.lblBaslik.Appearance.Options.UseTextOptions = true;
+            this.lblBaslik.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.lblBaslik.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lblBaslik.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.UltraFlat;
+            this.lblBaslik.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblBaslik.Location = new System.Drawing.Point(0, 0);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(1096, 47);
+            this.lblBaslik.TabIndex = 7;
+            this.lblBaslik.Text = "Kasa Seçim Ekranı";
+            // 
+            // FrmKasaSec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 515);
+            this.Controls.Add(this.gridContSecim);
+            this.Controls.Add(this.grpMenu);
+            this.Controls.Add(this.lblBaslik);
+            this.Name = "FrmKasaSec";
+            this.Text = "FrmKasaSec";
+            this.Load += new System.EventHandler(this.FrmKasaSec_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridContSecim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grpMenu)).EndInit();
+            this.grpMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraGrid.Columns.GridColumn colAciklama;
+        private DevExpress.XtraGrid.Columns.GridColumn colKasaGiris;
+        private DevExpress.XtraGrid.Columns.GridColumn colBakiye;
+        private DevExpress.XtraGrid.GridControl gridContSecim;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colKasaKodu;
+        private DevExpress.XtraGrid.Columns.GridColumn colKasaAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colYetkiliKodu;
+        private DevExpress.XtraGrid.Columns.GridColumn colYetkiliAdi;
+        private DevExpress.XtraGrid.Columns.GridColumn colKasaCikis;
+        private DevExpress.XtraEditors.GroupControl grpMenu;
+        private DevExpress.XtraEditors.SimpleButton btnSec;
+        private System.Windows.Forms.ImageList ımageList1;
+        private DevExpress.XtraEditors.SimpleButton btnKapat;
+        private DevExpress.XtraEditors.LabelControl lblBaslik;
+    }
+}
---
+++ SabzFara.BackOffice/Kasa/FrmKasaSec.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using SabzFara.Entities.DataAccess;
+using SabzFara.Entities.Context;
+
+namespace SabzFara.BackOffice.Kasa
+{
+    public partial class FrmKasaSec : DevExpress.XtraEditors.XtraForm
+    {
+        KasaDAL kasaDAL = new KasaDAL();
+        SabzFaraContext context = new SabzFaraContext();
+        public Entities.Tables.Kasa entity = new Entities.Tables.Kasa();
+        public FrmKasaSec()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmKasaSec_Load(object sender, EventArgs e)
+        {
+            gridContSecim.DataSource = kasaDAL.KasaListele(context);
+        }
+
+        private void btnSec_Click(object sender, EventArgs e)
+        {
+            string kasaKodu = gridView1.GetFocusedRowCellValue(colKasaKodu).ToString();
+          entity=  context.Kasalar.SingleOrDefault(c => c.KasaKodu == kasaKodu);
+            this.Close();
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
